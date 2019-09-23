6e6ed1879b56c309707536e47fa2f7d9a10c2774
Bruno Panassi <bhenrique95@outlook.com>
2019-09-23 08:09:47 -0300

    Add Logo Icon | Reajust Class Bloco Size | Change Scroll Effects | Add Responsive to Footer effect

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,16 @@
-var ContadorTextoH1 = 0
-var ContadorTextoH2 = 0
-
 var TextoH1 = 'Wake up, Neo...'
+var ContadorTextoH1 = 0
 var TextoH2 = 'The matrix has this website.'
-
+var ContadorTextoH2 = 0
 var Velocidade = 120
-
 var TimeOut
 
-var NivelOpacidade = 99 
 var EventoParm
 
 var ComponenteText = document.createElement("H2")
 ComponenteText.innerText = "More info"
 ComponenteText.style.color = "#1dd1a1"
-ComponenteText.style.fontFamily = 'Conv_BRI293', "sans-serif"
+ComponenteText.style.fontFamily = 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', "Arial", "sans-serif"
 ComponenteText.style.fontSize = "3em"
 ComponenteText.style.textAlign = "center"
 ComponenteText.style.opacity = "0"
@@ -31,34 +27,15 @@ ComponenteImgLink.appendChild(ComponenteImg)
 
 var ContactBox  = document.getElementById('Contact_Text_Box')  
 
-var ItsNotMorpheus_Opacity = 0
 var Skills_Opacity = 0
 
 window.onscroll = function() {Scroll()}
 
+window.onresize = function() {OnResize()}
+
 function Scroll()
 {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)
-    {   
-        var IntervaloFadeInMorpheus = setTimeout(FadeIn, 50)
-
-        function FadeIn()
-        {
-            if (ItsNotMorpheus_Opacity == 99)
-            {
-                clearTimeout(IntervaloFadeInMorpheus)
-            }
-            else
-            {
-                document.getElementById('Section_ItsNotMorpheus').style.opacity = (ItsNotMorpheus_Opacity/100).toString()
-                ItsNotMorpheus_Opacity++
-                
-                IntervaloFadeInMorpheus = setTimeout(FadeIn, 50)
-            }
-        }
-    }
-
-    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450)
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
     {
         var IntervaloFadeInSkills = setTimeout(FadeIn, 50)
 
@@ -86,6 +63,8 @@ function WaitToStart()
     ContactBox  = document.getElementById('Contact_Text_Box')
     ContactBox.appendChild(ComponenteText)
 
+    OnResize()
+
     TimeOut = setTimeout(TypeWriterH1, 1000)
 }
 
@@ -97,7 +76,12 @@ function ScrollToTheTop()
 
 function HoverEffect(EventoParm)
 {
-    FadeOutEffect()
+    var NivelOpacidade = 99 
+
+    if (ContactBox.offsetWidth > 1280)
+    {
+        FadeOutEffect()
+    }
 
     function FadeOutEffect()
     {
@@ -216,7 +200,72 @@ function ChangeSizeContainer()
 {
     var container = document.getElementsByClassName("cabecalho")[0]
 
+    var Cabecalho = document.getElementById("cabecalho")
+
     container.classList.add("ChangeSize")
 
     document.body.style.overflowY = "scroll"
+
+    var Intervalo = setTimeout(WaitTilResize, 50)
+
+    function WaitTilResize ()
+    {
+        if (Cabecalho.offsetHeight == 160)
+        {
+            clearTimeout(Intervalo)
+
+            var ItsNotMorpheus_Opacity = 0
+            var IntervaloFadeInMorpheus = setTimeout(FadeIn, 0)
+
+            function FadeIn()
+            {
+                if (ItsNotMorpheus_Opacity == 99)
+                {
+                    clearTimeout(IntervaloFadeInMorpheus)
+                }
+                else
+                {
+                    document.getElementById('Section_ItsNotMorpheus').style.opacity = (ItsNotMorpheus_Opacity/100).toString()
+                    ItsNotMorpheus_Opacity++
+                    
+                    IntervaloFadeInMorpheus = setTimeout(FadeIn, 0)
+                }
+            }
+        }
+        else
+        {
+            Intervalo = setTimeout(WaitTilResize, 50)
+        }
+    }
+}
+
+function OnResize()
+{
+    var Footer = document.getElementsByTagName("footer")[0]
+
+    if(ContactBox.offsetWidth < 1280)
+    {
+        ComponenteText.style.fontSize = "2em"
+        ComponenteImg.style.width = "48px"
+        ComponenteImg.style.height = "50px"
+        ComponenteImg.style.marginLeft = "calc((100% - 48px)/2)"
+
+        Footer.style.paddingTop = "26.8px"
+        ContactBox.appendChild(ComponenteImgLink)
+    }
+    else
+    {
+        ComponenteText.style.fontSize = "3em"
+        ComponenteImg.style.width = "68px"
+        ComponenteImg.style.height = "70px"
+        ComponenteImg.style.marginLeft = "calc((100% - 68px)/2)"
+        
+        Footer.style.paddingTop = "42.2px"
+
+        if(ContactBox.contains(ComponenteImgLink))
+        {
+            ContactBox.removeChild(ComponenteImgLink)
+        }
+
+    }
 }
